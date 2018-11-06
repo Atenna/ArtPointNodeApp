1a1bbfbc3366c312c7d495797cfff279d2b5dcfa
Ivana Lieskovska <idrabikova@gmail.com>
2018-11-06 22:07:33 +0100

    Map and layout setup

--- app.js
+++ app.js
@@ -7,13 +7,24 @@ var L = require('leaflet');
 // Initialize the map
 var map = L.map('map').setView([53.551086, 9.993682], 11);
 
-
-var esri_NatGeoWorldMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
-    maxZoom: 16
+// For different tile layers visit: https://leaflet-extras.github.io/leaflet-providers/preview/
+var Wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
+    maxZoom: 19
 }).addTo(map);
 
+var pinIcon = L.icon({
+    iconUrl: 'public/images/pin.png',
+    iconSize: [39, 39],
+    iconAnchor: [18, 39],
+    popupAnchor: [10, -35]
+});
 
+
+var marker = L.marker([53.551086, 9.993682], {icon: pinIcon}).addTo(map);
+marker.bindPopup("<b>Hamburg</b>");
+
+// when running app as node app.js without browserify
 function runServer(port)
 {
     var http = require('http');
